5476cc5aafbcbcf58081dfe5177aa6e19afac398
Jem <chrisvin1997@gmail.com>
2019-11-18 01:05:37 +0530

    Create DataValues.kt

---
+++ app/src/main/java/com/example/liquidswipedemo/DataValues.kt
@@ -0,0 +1,25 @@
+package com.example.liquidswipedemo
+
+import android.graphics.Color
+
+val backgroundColorArray: ArrayList<Int> = arrayListOf(
+    Color.BLUE,
+    Color.GREEN,
+    Color.CYAN,
+    Color.parseColor("#F6D336"),
+    Color.RED
+)
+val resourceArray: ArrayList<Int> = arrayListOf(
+    R.raw.mountain,
+    R.raw.map,
+    R.raw.luggage,
+    R.raw.camera,
+    R.raw.van
+)
+val titleArray: ArrayList<String> = arrayListOf(
+    "Hello fellow developer",
+    "If you like this library",
+    "Then do star it",
+    "And check out my other libraries",
+    "Cheers ^_^"
+)
