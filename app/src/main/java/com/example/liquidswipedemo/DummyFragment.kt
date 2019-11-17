348c123523285414984b80c4f759fe04e917c15b
Jem <chrisvin1997@gmail.com>
2019-11-18 01:47:50 +0530

    Refactor DummyFragment

--- app/src/main/java/com/example/liquidswipedemo/DummyFragment.kt
+++ app/src/main/java/com/example/liquidswipedemo/DummyFragment.kt
@@ -7,30 +7,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import com.airbnb.lottie.LottieAnimationView
+import com.airbnb.lottie.LottieDrawable
 import com.jem.easyreveal.RevealLayout
-import com.jem.easyreveal.clippathproviders.StarClipPathProvider
 import com.jem.liquidswipe.LiquidSwipeClipPathProvider
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+private const val ARG_BACKGROUND_COLOR = "param1"
+private const val ARG_RESOURCE = "param2"
+private const val ARG_TITLE = "param3"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [DummyFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DummyFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: Int? = null
-    private var param2: String? = null
+    private var param2: Int? = null
+    private var param3: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getInt(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            param1 = it.getInt(ARG_BACKGROUND_COLOR)
+            param2 = it.getInt(ARG_RESOURCE)
+            param3 = it.getString(ARG_TITLE)
         }
     }
 
@@ -41,27 +37,23 @@ class DummyFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_dummy, container, false).apply {
             setBackgroundColor(param1 ?: Color.RED)
-            findViewById<TextView>(R.id.fragment_textview).text = param2 ?: "Hello world!"
+            findViewById<TextView>(R.id.fragment_textview).text = param3 ?: "Hello fello developer!"
+            findViewById<LottieAnimationView>(R.id.lottieAnimationView).setAnimation(param2 ?: R.raw.mountain)
+            findViewById<LottieAnimationView>(R.id.lottieAnimationView).repeatCount = LottieDrawable.INFINITE
+            findViewById<LottieAnimationView>(R.id.lottieAnimationView).repeatMode = LottieDrawable.REVERSE
+            findViewById<LottieAnimationView>(R.id.lottieAnimationView).playAnimation()
             (this as? RevealLayout)?.clipPathProvider = LiquidSwipeClipPathProvider()
         }
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DummyFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: Int, param2: String) =
+        fun newInstance(param1: Int, param2: Int, param3: String) =
             DummyFragment().apply {
                 arguments = Bundle().apply {
-                    putInt(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                    putInt(ARG_BACKGROUND_COLOR, param1)
+                    putInt(ARG_RESOURCE, param2)
+                    putString(ARG_TITLE, param3)
                 }
             }
     }
