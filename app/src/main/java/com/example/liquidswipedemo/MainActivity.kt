43d3fc227ac31761dff2177479fa2ff06efb9cea
Jem <chrisvin1997@gmail.com>
2019-11-18 18:03:19 +0530

    Add touch interactivity related code in comments

--- app/src/main/java/com/example/liquidswipedemo/MainActivity.kt
+++ app/src/main/java/com/example/liquidswipedemo/MainActivity.kt
@@ -2,8 +2,7 @@ package com.example.liquidswipedemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.viewpager.widget.ViewPager
-import com.jem.liquidswipe.LiquidSwipePageTransformer
+import com.jem.liquidswipe.LiquidSwipeClipPathProvider
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +12,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         viewpager.adapter = CustomFragmentPagerAdapter(supportFragmentManager)
+
+        /*
+        // The following code can be used to dynamically change the waveCenterY
+        // based on the touch position (on the LiquidSwipeViewPager).
+
+        // The reason we don't do this internally is because,
+        // sometimes the viewpager layouts don't get the touch events
+        // when they are consumed directly by the viewpager.
+
+        val liquidSwipeClipPathProviders = Array(titleArray.count()) {
+            LiquidSwipeClipPathProvider()
+        }
+
+        //Similar logic can also be applied for your custom FragmentPagerAdapter
+        viewpager.adapter = CustomPagerAdapter(this, liquidSwipeClipPathProviders)
+
+        viewpager.setOnTouchListener { _, event ->
+            val waveCenterY = event.y
+            liquidSwipeClipPathProviders.map {
+                it.waveCenterY = waveCenterY
+            }
+            false
+        }
+        */
 
         // Create 20 times the number of actual pages, and start in the middle.
         // This way users can swipe left or right from the start.
