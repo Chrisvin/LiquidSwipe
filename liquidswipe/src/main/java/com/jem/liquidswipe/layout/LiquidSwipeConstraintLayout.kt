0a6ec658f678db1fcb3d440174bd8782661a63e5
Jem <chrisvin1997@gmail.com>
2019-11-18 21:02:44 +0530

    Refactor LiquidSwipeConstraintLayout
    Use internal LiquidSwipeLayout interface instead of the EasyRevealConstraintLayout, and make suitable changes.

--- liquidswipe/src/main/java/com/jem/liquidswipe/layout/LiquidSwipeConstraintLayout.kt
+++ liquidswipe/src/main/java/com/jem/liquidswipe/layout/LiquidSwipeConstraintLayout.kt
@@ -1,31 +1,59 @@
 package com.jem.liquidswipe.layout
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Path
 import android.util.AttributeSet
-import com.jem.easyreveal.layouts.EasyRevealConstraintLayout
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.jem.liquidswipe.base.ClipPathProvider
 import com.jem.liquidswipe.clippathprovider.LiquidSwipeClipPathProvider
 import com.jem.liquidswipe.base.LiquidSwipeLayout
 
-open class LiquidSwipeConstraintLayout : EasyRevealConstraintLayout,
-    LiquidSwipeLayout {
+/**
+ * `LiquidSwipeConstraintLayout` is a custom [ConstraintLayout] that implements [LiquidSwipeLayout].
+ */
+open class LiquidSwipeConstraintLayout : ConstraintLayout, LiquidSwipeLayout {
+    // Store path in local variable rather then getting it from ClipPathProvider each time
+    private var path: Path? = null
 
-    constructor(context: Context?) : super(context) {
-        initialize()
-    }
+    // Backing fields for LiquidSwipeLayout variables
+    private var _clipPathProvider: ClipPathProvider = LiquidSwipeClipPathProvider()
+    private var _currentRevealPercent: Float = 100f
 
-    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {
-        initialize()
-    }
+    override var clipPathProvider = _clipPathProvider
+    override var currentRevealPercent: Float
+        get() = _currentRevealPercent
+        set(value) {
+            revealForPercentage(value)
+        }
 
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
     constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(
         context,
         attrs,
         defStyleAttr
-    ) {
-        initialize()
+    )
+
+    /**
+     * Overriden from View
+     */
+    override fun draw(canvas: Canvas?) {
+        try {
+            canvas?.save()
+            path?.let {
+                canvas?.clipPath(it, clipPathProvider.op)
+            }
+            super.draw(canvas)
+        } finally {
+            canvas?.restore()
+        }
     }
 
-    private fun initialize() {
-        clipPathProvider = LiquidSwipeClipPathProvider()
+    override fun revealForPercentage(percent: Float) {
+        if (percent == _currentRevealPercent) return
+        _currentRevealPercent = percent
+        path = clipPathProvider.getPath(percent, this@LiquidSwipeConstraintLayout)
+        invalidate()
     }
 }
