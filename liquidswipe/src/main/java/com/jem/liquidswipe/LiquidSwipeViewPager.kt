5e69fbb8c0cad878c300f13d334d4d006b93be13
Jem <chrisvin1997@gmail.com>
2019-11-18 21:00:24 +0530

    Refactor LiquidSwipeViewPager

--- liquidswipe/src/main/java/com/jem/liquidswipe/LiquidSwipeViewPager.kt
+++ liquidswipe/src/main/java/com/jem/liquidswipe/LiquidSwipeViewPager.kt
@@ -5,7 +5,8 @@ import android.util.AttributeSet
 import android.view.View
 import android.view.animation.DecelerateInterpolator
 import androidx.viewpager.widget.ViewPager
-import com.jem.easyreveal.RevealLayout
+import com.jem.liquidswipe.base.LiquidSwipeLayout
+import com.jem.liquidswipe.util.FixedSpeedScroller
 import kotlin.math.abs
 
 class LiquidSwipeViewPager : ViewPager {
@@ -38,7 +39,7 @@ class LiquidSwipeViewPager : ViewPager {
         val mScroller = ViewPager::class.java.getDeclaredField("mScroller")
         mScroller.isAccessible = true
         val scroller = FixedSpeedScroller(context, DecelerateInterpolator())
-        scroller.duration = duration
+        scroller.scrollerDuration = duration
         mScroller.set(this, scroller)
     }
 
@@ -48,17 +49,17 @@ class LiquidSwipeViewPager : ViewPager {
 
     class LiquidSwipePageTransformer : ViewPager.PageTransformer {
         override fun transformPage(page: View, position: Float) {
-            if (page is RevealLayout) {
+            if (page is LiquidSwipeLayout) {
                 when {
                     position < -1 -> {
-                        page.revealForPercentage(0f, false)
+                        page.revealForPercentage(0f)
                     }
                     position < 0 -> {
                         page.translationX = -(page.width * position)
-                        page.revealForPercentage(100 - abs(position * 100), false)
+                        page.revealForPercentage(100 - abs(position * 100))
                     }
                     position <= 1 -> {
-                        page.revealForPercentage(100f, false)
+                        page.revealForPercentage(100f)
                         page.translationX = -(page.width * position)
                     }
                 }
