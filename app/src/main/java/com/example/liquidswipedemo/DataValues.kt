30a5a3985da3854bb671307498ada2f04e52ad34
Jem <chrisvin1997@gmail.com>
2019-11-18 16:43:59 +0530

    Update colors

--- app/src/main/java/com/example/liquidswipedemo/DataValues.kt
+++ app/src/main/java/com/example/liquidswipedemo/DataValues.kt
@@ -3,11 +3,11 @@ package com.example.liquidswipedemo
 import android.graphics.Color
 
 val backgroundColorArray: ArrayList<Int> = arrayListOf(
-    Color.BLUE,
-    Color.GREEN,
-    Color.CYAN,
+    Color.parseColor("#6200EE"),
+    Color.parseColor("#9FD29D"),
+    Color.parseColor("#AFE1F0"),
     Color.parseColor("#F6D336"),
-    Color.RED
+    Color.parseColor("#FA796B")
 )
 val resourceArray: ArrayList<Int> = arrayListOf(
     R.raw.mountain,
